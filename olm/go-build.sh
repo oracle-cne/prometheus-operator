8033881df20decb247024fc47d76291198d94e36
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2025-11-08 03:15:43 +0000

    Updated to v0.86.2 from https://github.com/prometheus-operator/prometheus-operator.git
    Generated from template at bd83905bd95fc02d5b13b1b6458a998625fe4eb5

---
+++ olm/go-build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+mkdir -p bin
+version="0.86.2"
+comp_prefix=prometheus
+
+GIT_REVISION=$(git rev-parse HEAD)
+BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+ldflags="
+        -X main.version=v${version}
+        -X github.com/prometheus/common/version.Version=${version}
+        -X github.com/prometheus/common/version.Revision=${GIT_REVISION}
+        -X github.com/prometheus/common/version.Branch=HEAD
+        -X github.com/prometheus/common/version.BuildUser=${USER}@${HOST}
+        -X github.com/prometheus/common/version.BuildDate=${BUILD_DATE}"
+
+compsInput=(operator prometheus-config-reloader admission-webhook)
+compsOutput=(prometheus-operator prometheus-config-reloader prometheus-admission-webhook)
+for i in ${!compsInput[@]}; do
+	go build -trimpath=false -v -o bin/${compsOutput[i]} \
+      -ldflags "${ldflags}" \
+      "${GOPATH_SRC}"/cmd/${compsInput[i]}
+  ./bin/${compsOutput[i]} --version
+done
