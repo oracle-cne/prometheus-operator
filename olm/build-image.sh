56871488448d2b066e07300e3b1a19242bfad5f7
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-05-06 04:27:25 +0000

    olm: use host networking for image builds
    Pass `--network=host` to `docker build` in `olm/build-image.sh` so the OLM
    image build step no longer depends on default container network setup and can
    complete in constrained build environments.

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -7,7 +7,9 @@ comp_prefix=prometheus
 tags=(${comp_prefix}-operator ${comp_prefix}-config-reloader ${comp_prefix}-admission-webhook)
 for i in ${!tags[@]}; do
   docker_tag="${registry}/${tags[i]}:v${version}"
+  # Use the host network to avoid netavark/iptables setup during image builds.
   docker build --pull \
+      --network=host \
       --build-arg https_proxy=${https_proxy} \
       -t ${docker_tag} -f ./olm/builds/Dockerfile.${tags[i]} .
   docker save -o ${tags[i]}.tar ${docker_tag}
